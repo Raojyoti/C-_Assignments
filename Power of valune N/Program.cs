87fc874afc32bde532ccefdb017bc200e5d58172
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 20:15:12 +0530

    Power of 2

---
+++ Power of valune N/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Power_of_valune_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=10, power =1 ;
+           
+             Console.Write("Enter number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+             for (int i=1;i<=num; i++)
+             {
+                 power = 2 * power;
+
+                 Console.WriteLine(i +" " + power);
+             } 
+            
+        }
+    }
+}
