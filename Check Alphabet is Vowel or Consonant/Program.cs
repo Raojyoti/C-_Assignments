6b60943d34ba0bd4f1574d5f32df563938e9e9bb
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 19:57:40 +0530

    Check Alphabet is Vowel or Consonant

---
+++ Check Alphabet is Vowel or Consonant/Program.cs
@@ -0,0 +1,37 @@
+﻿/* C# Program to Check Whether an Alphabet is Vowel or Consonant   */
+
+using System;
+
+namespace Check_Alphabet_is_Vowel_or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Character: ");
+            char ch =Convert.ToChar(Console.ReadLine());
+            switch (ch) 
+            {
+                 case 'a':
+                     Console.WriteLine(ch + " is vowel");
+                      break;
+                case 'e':
+                    Console.WriteLine(ch + " is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine(ch + " is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine(ch + " is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine(ch + " is vowel");
+                    break;
+                default:
+                    Console.WriteLine(ch + " is Consonant");
+                    break;
+
+            }
+        }
+    }
+}
