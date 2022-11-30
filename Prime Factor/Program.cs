7865f601d08ae106486d4f4a0082612095ad9bf7
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 20:17:50 +0530

    Prime Factor

---
+++ Prime Factor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prime_Factor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
