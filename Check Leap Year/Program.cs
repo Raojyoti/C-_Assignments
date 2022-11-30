a88167acdabb4302cf7c1ebcee6cddfca9a50298
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 19:47:18 +0530

    Check Leap Year

---
+++ Check Leap Year/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Check Leap Year or not */
+
+using System;
+
+namespace Check_Leap_Year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter four digit year value: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(year + " is Leap Year ");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is Leap Year ");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year + " is not Leap Year ");
+            }
+            else 
+            {
+                Console.WriteLine(year + " is not Leap Year ");
+            }
+        }
+    }
+}
