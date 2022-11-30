266d62a293c1a2a1dcdc40327d60e3a3ec71ffd6
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 20:00:43 +0530

    Compute Quotient and Remainder

---
+++ Compute Quotient and Remainder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Compute_Quotient_and_Remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of Dividend :  ");
+            double Dividend = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the values of Divisor:  ");
+            double Divisor = Convert.ToDouble(Console.ReadLine());
+            double Quotient = Dividend / Divisor;
+            double Remainder = Dividend % Divisor;
+            Console.WriteLine("Quotient is: " + Quotient);
+            Console.WriteLine("Remainder is: " + Remainder);
+
+        }
+    }
+}
