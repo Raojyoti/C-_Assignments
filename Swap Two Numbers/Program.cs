f0b8302cc1c3445443a9a945257d524dedf32666
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-11-30 20:20:35 +0530

    Swap Two Numbers

---
+++ Swap Two Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  C# Program to Swap Two Numbers     */
+
+using System;
+
+namespace Swap_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter values of a and b : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+             a = a + b;
+             b = a - b; 
+             a = a - b;  
+
+            Console.WriteLine("Values of a and b after swapping: {0} {1} ", a, b);
+
+        }
+    }
+}
